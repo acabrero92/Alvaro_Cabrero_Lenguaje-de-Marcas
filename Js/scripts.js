eee95863c4355c1e71615aed6d11daf553e0e796
acabrero92 <alvarocabrero92@gmail.com>
2024-07-12 13:07:23 +0200

    upgrade 12/07

--- Js/scripts.js
+++ Js/scripts.js
@@ -1,13 +1,12 @@
-// scripts.js
-
 function calcularPresupuesto() {
     const cantidad = document.getElementById('cantidad').value;
     const precioUnitario = 50;
     let total = cantidad * precioUnitario;
 
     if (cantidad >= 3) {
-        total *= 0.90; // Aplicar un 10% de descuento
+        total *= 0.9; // Aplicar 10% de descuento
     }
 
-    document.getElementById('resultado').innerText = `Total: €${total.toFixed(2)}`;
+    document.getElementById('resultado').textContent = `El precio total es: ${total.toFixed(2)}€`;
+    return false; // Evitar que el formulario se envíe
 }
